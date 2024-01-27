35daa1d2f23d6330dc23a054ae2a18d1c8a1c5af
rohit kumar <rraj70775@gmail.com>
2024-01-28 00:02:25 +0530

    changes cors disable in security config

--- blog-app-api/src/main/java/com/blog/config/SecurityConfig.java
+++ blog-app-api/src/main/java/com/blog/config/SecurityConfig.java
@@ -107,25 +107,25 @@ public class SecurityConfig {
 
 //	}
 	
-	@Bean
-	public FilterRegistrationBean<CorsFilter> coresFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.addAllowedOriginPattern("*");
-		corsConfiguration.addAllowedHeader("Authorization");
-		corsConfiguration.addAllowedHeader("Content-Type");
-		corsConfiguration.addAllowedHeader("Accept");
-		corsConfiguration.addAllowedMethod("POST");
-		corsConfiguration.addAllowedMethod("GET");
-		corsConfiguration.addAllowedMethod("DELETE");
-		corsConfiguration.addAllowedMethod("PUT");
-		corsConfiguration.addAllowedMethod("OPTIONS");
-		corsConfiguration.setMaxAge(3000L);
-		source.registerCorsConfiguration("/**", corsConfiguration);
-		FilterRegistrationBean<CorsFilter> bean =new FilterRegistrationBean<CorsFilter>(new CorsFilter(source));
-		return bean;
-	}
+//	@Bean
+//	public FilterRegistrationBean<CorsFilter> coresFilter() {
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		CorsConfiguration corsConfiguration = new CorsConfiguration();
+//		corsConfiguration.setAllowCredentials(true);
+//		corsConfiguration.addAllowedOriginPattern("*");
+//		corsConfiguration.addAllowedHeader("Authorization");
+//		corsConfiguration.addAllowedHeader("Content-Type");
+//		corsConfiguration.addAllowedHeader("Accept");
+//		corsConfiguration.addAllowedMethod("POST");
+//		corsConfiguration.addAllowedMethod("GET");
+//		corsConfiguration.addAllowedMethod("DELETE");
+//		corsConfiguration.addAllowedMethod("PUT");
+//		corsConfiguration.addAllowedMethod("OPTIONS");
+//		corsConfiguration.setMaxAge(3000L);
+//		source.registerCorsConfiguration("/**", corsConfiguration);
+//		FilterRegistrationBean<CorsFilter> bean =new FilterRegistrationBean<CorsFilter>(new CorsFilter(source));
+//		return bean;
+//	}
 
 
 
